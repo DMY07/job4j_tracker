9d90e33a2c0748303db64b613f28c4751c2891be
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-06-07 20:14:41 +0500

    7. Метод удаления заявки Tracker.delete [#211749 #499492]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -57,13 +57,12 @@ public class Tracker {
         return false;
     }
 
-    public boolean delete(int id) {
+    public void delete(int id) {
         int index = indexOf(id);
         if (index != -1) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
-            items[--size] = null;
-            return true;
+            items[size - 1] = null;
+            size--;
         }
-        return false;
     }
 }
