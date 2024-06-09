849649e87bcbb63a4ccd67fc76b7d5a86643c40f
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-06-09 17:22:43 +0500

    8. Что такое валидация? [#246864 #499495]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -47,19 +47,16 @@ public class Tracker {
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
         boolean result = index != -1;
-        if (index != -1) {
+        if (result) {
             item.setId(id);
             items[index] = item;
-            return true;
         }
-        return false;
+        return result;
     }
 
     public void delete(int id) {
         int index = indexOf(id);
-        if (index == -1) {
-            System.out.println("Элемент не найден");
-        } else {
+        if (index != -1) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
