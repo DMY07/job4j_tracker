2dedab36ead852872317a2a08f05f79452a94f24
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-06-09 18:41:58 +0500

    2.0. Чтение из консоли. Класс Scanner. [#179514]

---
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -0,0 +1,17 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class MagicBall {
+
+    private static final String[] answers = {"Да", "Нет", "Может быть"};
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
+        String question = input.nextLine();
+        int answer = new Random().nextInt(3);
+        System.out.println("Ответ великого Оракула: " + answers[answer]);
+    }
+}
