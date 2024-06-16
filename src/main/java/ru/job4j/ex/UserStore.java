aa82881b3bfb798a75ee3f932aa766533c41faf8
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-06-16 18:49:15 +0500

    0.5. Иерархия исключений и множественный catch. [#219368]

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,41 @@
+package ru.job4j.ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        for (User user : users) {
+            if (user.getUsername().equals(login)) {
+                return user;
+            }
+        }
+        throw new UserNotFoundException("Пользователь не найден");
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid()) {
+            throw new UserInvalidException("Пользователь не валидный");
+        }
+        if (user.getUsername().length() < 3) {
+            throw new UserInvalidException("Имя пользователя должно содержать не менее 3 символов");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true),
+                new User("Pa", true),          // Example of invalid user (username < 3 characters)
+                new User("John Doe", false)    // Example of invalid user (not valid)
+        };
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has access");
+            }
+        } catch (UserInvalidException e) {
+            System.out.println("Пользователь не валидный");
+        } catch (UserNotFoundException e) {
+            System.out.println("Пользователь не найден");
+        }
+    }
+}
