a43a3b4de4ba9ecc9152456c47dc56ce0c673336
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-06-24 22:20:28 +0500

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PassportOfficeTest {
+    @Test
+    public void whenTestAddMethod() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+    }
+
+    @Test
+    public void whenAddDuplicatePassportThenFalse() {
+        PassportOffice office = new PassportOffice();
+        Citizen citizen1 = new Citizen("12345", "Ryan Gosling");
+        Citizen citizen2 = new Citizen("12345", "Ryan Gosling");
+        office.add(citizen1);
+        boolean result = office.add(citizen2);
+        assertThat(result).isFalse();
+    }
+}
