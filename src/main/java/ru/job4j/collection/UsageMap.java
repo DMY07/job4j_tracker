49cc291890cbd310f2c0da6b8f21ef637fc2232c
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-06-25 22:27:08 +0500

    0. Map, HashMap [#212652 #502213]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,10 @@ public class UsageMap {
         HashMap<String, String> map = new HashMap<>();
         map.put("parsentev@yandex.ru", "Petr Arsentev");
         map.put("dpl_info@mail.ru", "Дмитрий");
+        map.put("test@mail.ru", "Василий");
+        map.put("test1@mail.ru", "Григорий");
+        map.put("ttt@mail.ru", "Егор");
+        map.put("ttt@mail.ru", "Игорь");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
