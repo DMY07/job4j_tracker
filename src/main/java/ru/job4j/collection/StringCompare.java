7c0f83197d912e71108ec44d863a3c28f0a6bb00
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-06-27 20:54:44 +0500

    3. Компаратор для строк [#35008 #502522]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -7,9 +7,7 @@ public class StringCompare implements Comparator<String> {
     public int compare(String left, String right) {
         int minLength = Math.min(left.length(), right.length());
         for (int i = 0; i < minLength; i++) {
-            char leftChar = left.charAt(i);
-            char rightChar = right.charAt(i);
-            int charComparison = Character.compare(leftChar, rightChar);
+            int charComparison = Character.compare(left.charAt(i), right.charAt(i));
             if (charComparison != 0) {
                 return charComparison;
             }
