6ab4eec269b3514956298f62d482006738ec0040
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-07-02 19:51:16 +0500

    3. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -8,12 +8,6 @@ public class DepartmentsDescComparator implements Comparator<String> {
         String[] leftParts = left.split("/");
         String[] rightParts = right.split("/");
         int result = rightParts[0].compareTo(leftParts[0]);
-        int minLength = Math.min(left.length(), right.length());
-        for (int i = 0; i < minLength; i++) {
-            if (left.charAt(i) != right.charAt(i)) {
-                return result != 0 ? result : left.compareTo(right);
+        return result != 0 ? result : left.compareTo(right);
             }
         }
-        return left.length() - right.length();
-    }
-}
