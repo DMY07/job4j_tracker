1e3312ab5da10200a106d5947b13521377f7174c
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-07-24 01:29:05 +0500

    1. Метод .of [#70623 #506356]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -15,11 +15,11 @@ public class PhoneDictionary {
         Predicate<Person> surnamePredicate = person -> person.getSurname().contains(key);
         Predicate<Person> phonePredicate = person -> person.getPhone().contains(key);
         Predicate<Person> addressPredicate = person -> person.getAddress().contains(key);
-        Predicate<Person> combine = namePredicate.or(surnamePredicate)
+        var combine = namePredicate.or(surnamePredicate)
                 .or(phonePredicate)
                 .or(addressPredicate);
         ArrayList<Person> result = new ArrayList<>();
-        for (Person person : persons) {
+        for (var person : persons) {
             if (combine.test(person)) {
                 result.add(person);
             }
--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -10,19 +10,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Petr");
+        var persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
     }
 
     @Test
     void whenNoResult() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
-        List<Person> persons = phones.find("No_Key");
+        var persons = phones.find("No_Key");
         assertThat(persons).isEmpty();
     }
 
